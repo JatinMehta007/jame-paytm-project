1fdf8754115b617503d92a3bc43a744711f30474
Jatin_Mehta <mehtajatin045@gmail.com>
2025-03-08 22:34:06 +0530

    add transaction

--- apps/user-app/components/AddMoneyCard.tsx
+++ apps/user-app/components/AddMoneyCard.tsx
@@ -1,6 +1,11 @@
 "use client"
 import { Button } from "@repo/ui/button";
 import { Card } from "@repo/ui/card";
+import { useState } from "react";
+import { Select } from "@repo/ui/select"; 
+import { TextInput } from "@repo/ui/textinput";
+import { createOnRampTransactions } from "../app/lib/actions/CreateOnRamptxn";
+
 
 
 const SUPPORTED_BANKS = [{
@@ -11,24 +16,29 @@ const SUPPORTED_BANKS = [{
     redirectUrl: "https://www.axisbank.com/"
 }];
 
-export const AddMoney = () => {
+export const  AddMoney = () => {
     const [redirectUrl, setRedirectUrl] = useState(SUPPORTED_BANKS[0]?.redirectUrl);
+    const [amount, setAmount] = useState(0 );
+    const [provider, setProvider] = useState (SUPPORTED_BANKS[0]?.name || "");
+
     return <Card title="Add Money">
     <div className="w-full">
-        <TextInput label={"Amount"} placeholder={"Amount"} onChange={() => {
-
+        <TextInput label={"Amount"} placeholder={"Amount"} onChange={(value) => {
+             setAmount(value)
         }} />
         <div className="py-4 text-left">
             Bank
         </div>
         <Select onSelect={(value) => {
-            setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "")
+            setRedirectUrl(SUPPORTED_BANKS.find(x => x.name === value)?.redirectUrl || "");
+            setProvider(SUPPORTED_BANKS.find(x => x.name === value)?.name || "")
         }} options={SUPPORTED_BANKS.map(x => ({
             key: x.name,
             value: x.name
         }))} />
         <div className="flex justify-center pt-4">
-            <Button onClick={() => {
+            <Button onClick={async() => {
+                await createOnRampTransactions(amount * 100, provider)
                 window.location.href = redirectUrl || "";
             }}>
             Add Money
--- packages/ui/src/code.tsx
+++ packages/ui/src/code.tsx
@@ -1,3 +1,4 @@
+
 export function Code({
   children,
   className,
