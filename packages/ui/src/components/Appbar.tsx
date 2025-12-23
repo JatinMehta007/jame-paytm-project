5466dce81405406808b2b2cb2e98fd2c8a0bdd86
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-23 18:17:56 +0530

    add logo which was created on figma

--- packages/ui/src/components/Appbar.tsx
+++ packages/ui/src/components/Appbar.tsx
@@ -16,10 +16,10 @@ export const Appbar = ({
     onSignin,
     onSignout
 }: AppbarProps) => {
-    return <div className="flex justify-between border-b px-4 bg-white">
+    return <div className="flex justify-between items-center border-b px-4 ">
         <div className="text-lg flex  justify-center  items-center">
-            <img src="/images/logo.png" alt="Logo" className="h-[40px] "/>
-            <p className="justify-center items-center flex pl-1">Jame</p>
+            <img src="/images/lofo.svg" alt="Logo" className="h-[40px] "/>
+            <p className="items-center flex pl-1 text-2xl  font-title text-green-600">Jame</p>
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
