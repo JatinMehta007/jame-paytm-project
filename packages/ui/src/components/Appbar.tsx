87a6b95bf43d13417dce798697ba83de711cfeec
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-24 23:19:27 +0530

    add new logo

--- packages/ui/src/components/Appbar.tsx
+++ packages/ui/src/components/Appbar.tsx
@@ -18,7 +18,7 @@ export const Appbar = ({
 }: AppbarProps) => {
     return <div className="flex justify-between bg-white/10 items-center px-4 ">
         <div className="text-lg flex  justify-center  items-center">
-            <img src="/images/lofo.svg" alt="Logo" className="h-[40px] "/>
+            <img src="/images/logos.svg" alt="Logo" className="h-[40px] "/>
             <p className="items-center flex pl-1 text-2xl  font-title text-green-600">Jame</p>
         </div>
         <div className="flex flex-col justify-center pt-2">
