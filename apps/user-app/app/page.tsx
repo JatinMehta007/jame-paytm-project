70e176c627651eb8191e2ea5da9e8936b6680b3b
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-24 23:15:31 +0530

    add Footer

--- apps/user-app/app/page.tsx
+++ apps/user-app/app/page.tsx
@@ -23,18 +23,18 @@ export default async function Page() {
 <div>
   <LampSection></LampSection>
 </div>
-<div>
+
   <div className="flex flex-col mt-20 ">
       <p className="">Your digital card</p>
       <p className="text-gray-300"> <span className="text-white/30">Built for a</span> borderless world</p>
       <div className="  w-32 text-center
     rounded-full border border-white/20 px-4 py-1
     text-xs uppercase tracking-widest text-white/60
-    backdrop-blur-md">
+    bg-white/10">
     Coming Soon
       </div>
       </div>
-</div>
+
     </div>
 
     <Footer></Footer>
