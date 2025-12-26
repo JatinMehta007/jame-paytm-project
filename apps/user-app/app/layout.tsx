a7c43b918053e4178c74a8a5a40b3792830d5483
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-26 19:13:25 +0530

    add toaster

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -2,7 +2,6 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
-import { AppbarClient } from "../components/AppbarClient";
 import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
