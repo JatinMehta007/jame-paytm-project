0304d93e3c08c4dc06e15bb69ed8ab90cd0a4e12
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-26 13:29:56 +0530

    add Toaster

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Providers } from "../provider";
 import { AppbarClient } from "../components/AppbarClient";
+import { Toaster } from "react-hot-toast";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,8 @@ export default function RootLayout({
       <Providers>
         <body className={inter.className}>
           <div className="min-w-screen min-h-screen bg-[#ebe6e6]">
-            <AppbarClient />
             {children}
+            <Toaster position="top-center" />
           </div>
         </body>
       </Providers>
