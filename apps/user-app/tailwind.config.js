b685eb9c5d578722295bf93a191a4ec33418d34a
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-26 19:18:23 +0530

    add font

--- apps/user-app/tailwind.config.js
+++ apps/user-app/tailwind.config.js
@@ -1,4 +1,11 @@
 
+const defaultTheme = require("tailwindcss/defaultTheme");
+ 
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -11,7 +18,11 @@ module.exports = {
     extend: {
       fontFamily:{
         title:["Luckiest Guy", "cursive"],
-      }
+        // titles: ["BBH Bartle", "sans-serif"]
+      },
+      boxShadow: {
+        input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
     },
   },
   plugins: [],
