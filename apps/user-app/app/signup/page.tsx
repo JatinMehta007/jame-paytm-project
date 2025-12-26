af7b754e87daf9d920e3ba1af7a4352180741213
Jatin_Mehta <mehtajatin045@gmail.com>
2025-12-26 13:33:38 +0530

    add signup page

---
+++ apps/user-app/app/signup/page.tsx
@@ -0,0 +1,198 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+import {signupSchema} from "@repo/types/signupSchema";
+import axios from "axios";
+import { cn } from "../../../../packages/ui/lib/utils";
+import {
+    IconBrandGithub,
+    IconBrandGoogle,
+  } from "@tabler/icons-react";
+import { Label } from "@repo/ui/label";
+import { Input }  from "@repo/ui/input";
+import {Spinner} from "@repo/ui/spinner";
+import toast from "react-hot-toast";
+import { Eye, EyeOff } from "lucide-react";
+
+export default function Signup(){
+    const router = useRouter();
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+    const [phoneNumber , setPhoneNumber] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [showPassword, setShowPassword]  = useState(false);
+    // const [error, setError] = useState("");
+    const [validationErrors, setValidationErrors] = useState<any>(null); // For displaying validation errors
+
+    const handleSignup = async ()=>{
+        // setError("");
+        setValidationErrors(null)
+
+        const result = signupSchema.safeParse({
+            name,
+            email,
+            phoneNumber,
+            password
+        })
+
+        if(!result.success){
+            setValidationErrors(result.error.format());
+            console.log(result.error.format());
+            return;
+        }
+
+        try{
+          setIsLoading(true);
+            const response = await axios.post("/api/user",{
+                name,
+                email,
+                phoneNumber,
+                password
+            });
+            toast.success("Account created successfully");
+            router.push("/login");
+        } catch(error : any){
+            if(error.response && error.response.status === 400){
+                toast.error("Phone number is already exist");
+            } else{
+                console.log("Error while creating a account", error);
+                toast.error("Something went wrong. Please try again later");
+            }
+        } finally{
+          setIsLoading(false);
+        }
+    };
+    
+    if(isLoading){
+       // Render the spinner as a full-page overlay
+       return (
+        <div className="h-screen flex justify-center items-center">
+          <Spinner />
+        </div>
+      );
+
+    }
+    return (
+        <div className="shadow-input relative top-6   mx-auto w-full max-w-md rounded-none bg-white p-4 md:rounded-2xl md:p-8 dark:bg-black">
+      <h2 className="text-xl text-center font-bold text-neutral-800 dark:text-neutral-200">
+        Welcome to <span className="text-green-600 ">Jame</span>
+      </h2>
+      <p className="mt-2 max-w-sm text-sm text-neutral-600 dark:text-neutral-300">
+        Login to Jame if you can because we don&apos;t have a <span className="text-green-600">login</span> flow
+        yet
+      </p>
+ 
+      <form className="my-8" onSubmit={(e) => {
+        e.preventDefault();
+        handleSignup();
+      }}>
+        <div className="mb-4 flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2">
+          <LabelInputContainer>
+            <Label htmlFor="firstname">Name</Label>
+            <Input id="firstname" placeholder="Jatin" type="text" value={name} onChange={(e)=>{
+                setName(e.target.value)
+            }} />{validationErrors?.name && <p className="text-red-600">{validationErrors.name._errors[0]}</p>}
+          </LabelInputContainer>
+        </div>
+        
+        <LabelInputContainer className="mb-4">
+          <Label htmlFor="email">Email Address</Label>
+          <Input id="email" placeholder="programmer@gmail.com" type="email" value={email} onChange={(e)=>{
+            setEmail(e.target.value)    
+          }} />{validationErrors?.email && <p className="text-red-600">{validationErrors.email._errors[0]}</p>}
+        </LabelInputContainer>
+
+        <LabelInputContainer className="mb-8">
+          <Label htmlFor="phoneNumber">Phone Number</Label>
+          <Input
+            id="phoneNumber"
+            placeholder="88245XXXXX"
+            type="tel"
+            value={phoneNumber}
+            onChange={(e) => {
+                setPhoneNumber(e.target.value);
+            }}
+          />
+          {validationErrors?.phoneNumber && <p className="text-red-600">{validationErrors.phoneNumber._errors[0]}</p>}
+        </LabelInputContainer>
+        <LabelInputContainer className="mb-4 relative">
+          <Label htmlFor="password" >Password</Label>
+          <Input id="password" placeholder="••••••••" 
+              type={showPassword ? "text" : "password"} value={password} onChange={(e)=>{
+            setPassword(e.target.value) 
+          }}/>
+          
+          <span
+           onClick={() => setShowPassword((prev) => !prev)}
+             className="absolute right-3 top-[36px] -translate-y-1/2 cursor-pointer text-gray-500"
+            >
+             {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+            </span>
+
+          {validationErrors?.password && <p className="text-red-600">{validationErrors.password._errors[0]}</p>}
+        </LabelInputContainer>
+ 
+        <button
+          className="group/btn relative block h-10 w-full rounded-md bg-gradient-to-br from-black to-neutral-600 font-medium text-white shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:bg-zinc-800 dark:from-zinc-900 dark:to-zinc-900 dark:shadow-[0px_1px_0px_0px_#27272a_inset,0px_-1px_0px_0px_#27272a_inset]"
+          type="submit"
+        >
+          Sign up &rarr;
+          <BottomGradient />
+        </button>
+ 
+        <div className="my-8 h-[1px] w-full bg-gradient-to-r from-transparent via-neutral-300 to-transparent dark:via-neutral-700" />
+ 
+        <div className="flex flex-col space-y-4">
+          <button
+            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+            type="button"
+          >
+            <IconBrandGithub className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-sm text-neutral-700 dark:text-neutral-300">
+              GitHub
+            </span>
+            <BottomGradient />
+          </button>
+          <button
+            className="group/btn shadow-input relative flex h-10 w-full items-center justify-start space-x-2 rounded-md bg-gray-50 px-4 font-medium text-black dark:bg-zinc-900 dark:shadow-[0px_0px_1px_1px_#262626]"
+            type="button"
+          >
+            <IconBrandGoogle className="h-4 w-4 text-neutral-800 dark:text-neutral-300" />
+            <span className="text-sm text-neutral-700 dark:text-neutral-300">
+              Google
+            </span>
+            <BottomGradient />
+          </button>
+
+        </div>
+      </form>
+    </div>
+      );
+    }
+
+     
+const BottomGradient = () => {
+    return (
+      <>
+        <span className="absolute inset-x-0 -bottom-px block h-px w-full bg-gradient-to-r from-transparent via-cyan-500 to-transparent opacity-0 transition duration-500 group-hover/btn:opacity-100" />
+        <span className="absolute inset-x-10 -bottom-px mx-auto block h-px w-1/2 bg-gradient-to-r from-transparent via-indigo-500 to-transparent opacity-0 blur-sm transition duration-500 group-hover/btn:opacity-100" />
+      </>
+    );
+  };
+   
+  const LabelInputContainer = ({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }) => {
+    return (
+      <div className={cn("flex w-full flex-col space-y-2", className)}>
+        {children}
+      </div>
+    );
+  };
+ 
